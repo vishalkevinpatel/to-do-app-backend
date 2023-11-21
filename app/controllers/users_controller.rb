b88832cea0bcc9a7b2cec766700cbe87c036e8e5
Derek McGee <dmcgee2001@gmail.com>
2023-11-21 12:54:06 -0500

    adds seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,17 +10,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(
-      name: params["name"],
-      email: params["email"],
-      password: params["password"],
-      password_confirmation: params["password_confirmation"],
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
     )
-    @user.save
-    if @user.valid?
-      render :show
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
 
