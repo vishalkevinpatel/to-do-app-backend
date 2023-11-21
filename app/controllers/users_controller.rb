d364c002098bc2ed0f7851470335bf05452f0447
Derek McGee <dmcgee2001@gmail.com>
2023-11-21 12:45:11 -0500

    adds user crd action methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params["id"])
+    render :show
+  end
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def create
+    @user = User.new(
+      name: params["name"],
+      email: params["email"],
+      password: params["password"],
+      password_confirmation: params["password_confirmation"],
+    )
+    @user.save
+    if @user.valid?
+      render :show
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params["id"])
+    user.delete
+    render json: { message: "successfully deleted" }
+  end
 end
